76384fb2c55d45a647156fc2c832d2e3d60c240d
Marian <mariannguyeen@hotmail.com>
2020-06-19 09:45:21 -0700

    debugging database

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ app.get('/database',(req,res) => {
   
 });
 
-app.get('/add', (req, res) => {
-  console.log("adding customer");
-  res.render('pages/adduser')
-});
-
 app.post('/adduser', (req, res)=>{
   console.log("post request for /adduser");
   var uname = req.body.uname;
