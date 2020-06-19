20cd8ca92f9cbb1d648fd6d8e557ba9bc069cf2c
Marian <mariannguyeen@hotmail.com>
2020-06-18 21:01:42 -0700

    connected to database

--- public/advanced.js
+++ public/advanced.js
@@ -3,25 +3,27 @@
 // function - var
 // block - let
 
-/*
-function myfunc(){
-    v = "myfunc_var";
-    let le = "myfunc_let";
-    console.log(v);
-    console.log(le);
-    for (let i = 0, i<10, i++){
-        console.log(i);
-    }
-    // console.log(i);
-}
 
-myfunc();
-console.log(v);
+// function myfunc(){
+//     v = "myfunc_var";
+//     let le = "myfunc_let";
+//     console.log(v);
+//     console.log(le);
+//     for (let i = 0; i<10; i++){
+//         console.log(i);
+//     }
+//     // console.log(i);
+// }
 
-*/
+// myfunc();
+// console.log(v);
+
+
 
 var cb1 = ()=> {console.log("cb1");}
 var cb2 = ()=> {console.log("cb2");}
 
 console.log("start...");
-setTimeout()
+setTimeout(cb1, 1000);
+setTimeout(cb2, 500);
+console.log("end...");
--- public/script.js
+++ public/script.js
@@ -5,3 +5,55 @@ function func1 (a,b,c){
 var func2 = function(){
     console.log("hello world");
 }
+
+// func2();
+
+function asc(a,b){
+    return a-b;
+}
+
+var arr = [10, 22, 13, 54, 5, 69];
+arr.sort(asc);
+
+// for (item in arr) {
+//  console.log(arr[item]);    
+// }
+
+var x = arr.every(function(val){
+    return val > 7;
+});
+
+console.log(x);
+
+// -- objects
+
+// var person = { 
+//     firstname: "Mar",
+//     lastname: "Nguyen",
+//     id: 1234,
+//     fullname: function(){
+//         return this.firstname + " " + this.lastname;
+//     }
+//  }
+
+// function Person(f, l, i, a)
+// {
+//     this.firstname = f;
+//     this.lastname = l;
+//     this.age = a;
+//     this.id = i;
+// }
+
+// -- event handlers
+function runcommand(){
+    document.getElementsByTagName("h1")[0].innerHTML = "ADD USERS";
+}
+document.getElementById("button").onclick = runcommand;
+
+// -- event listeners
+document.getElementById("button").addEventListener('click', (evt)=>{console.log("hello")});
+document.getElementById("button").addEventListener('click', (evt)=>{console.log("world")});
+
+window.addEventListener('keypress', (evt)=>{console.log(evt.keyCode)});
+
+
